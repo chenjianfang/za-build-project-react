72aa1b30412a8893e7becb1204de6022edee4031
chenjianfang <jianfang.chen@zhenai.com>
2020-11-08 14:04:48 +0800

    feat postcss插件一定要固定版本号！！！

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,24 @@
-const postcssPresetEnv = require('postcss-preset-env'); // 使您可以将现代CSS转换为大多数浏览器可以理解的内容，包含autoprefixer
-const precss = require('precss'); // scss功能嵌套、混合
-const px2rem = require('postcss-px2rem'); // scss功能嵌套、混合
-
 module.exports = {
     plugins: [
-        postcssPresetEnv({
-            autoprefixer: true
+        require('postcss-import')({
+            path: ['./src']
         }),
-        precss,
-        px2rem({
+        require('postcss-px2rem')({
             remUnit: 75
         }),
+        require('cssnano'),
+        require('precss'),
+        require('postcss-for'),
+        require('postcss-preset-env')({
+            autoprefixer: true
+        }),
+        require('postcss-nested'),
+        require('postcss-extend'),
+        require('postcss-simple-vars'),
+        require('postcss-mixins'),
     ]
 };
+
 
 
 
