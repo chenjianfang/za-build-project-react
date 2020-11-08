d21c04249ec280bddf3b066c6256a767a5d6cee4
chenjianfang <jianfang.chen@zhenai.com>
2020-11-08 12:34:00 +0800

    修改配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,19 @@
+const postcssPresetEnv = require('postcss-preset-env'); // 使您可以将现代CSS转换为大多数浏览器可以理解的内容，包含autoprefixer
+const precss = require('precss'); // scss功能嵌套、混合
+const px2rem = require('postcss-px2rem'); // scss功能嵌套、混合
+
 module.exports = {
     plugins: [
-        // eslint-disable-next-line global-require
-        require('autoprefixer'),
-        // eslint-disable-next-line global-require
-        // require('postcss-px2rem')({
-        //     remUnit: 75
-        // }),
+        postcssPresetEnv({
+            autoprefixer: true
+        }),
+        precss,
+        px2rem({
+            remUnit: 75
+        }),
     ]
-}
+};
+
+
+
+
